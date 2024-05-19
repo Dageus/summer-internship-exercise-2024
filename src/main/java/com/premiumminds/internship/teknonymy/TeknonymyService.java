fa59bce1c6bc77a91bb483eb9e01257d820f83d8
Dageus <101069446+Dageus@users.noreply.github.com>
2024-05-19 14:59:47 +0100

    decided on the search, the rest is trivial

--- src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
+++ src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
@@ -4,6 +4,15 @@ import com.premiumminds.internship.teknonymy.Person;
 
 class TeknonymyService implements ITeknonymyService {
 
+  private static String _firstGenMale     = "father";
+  private static String _firstGenFemale   = "mother";
+  private static String _secondGenMale    = "grandfather";
+  private static String _secondGenFemale  = "grandmother";
+  private static String _prefix           = "great-";
+
+  /* opting for a BFS since we don't know the full depth */
+  private Queue<Person> _queue = new LinkedList<Person>();
+
   /**
    * Method to get a Person Teknonymy Name
    * 
