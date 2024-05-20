abf96d22f1cb8a6670221e7f8b9c3ff24e39d2f4
Dageus <101069446+Dageus@users.noreply.github.com>
2024-05-20 11:10:50 +0100

    search choice wasn't quite right, this is a tree not a graph

--- src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
+++ src/main/java/com/premiumminds/internship/teknonymy/TeknonymyService.java
@@ -1,6 +1,8 @@
 package com.premiumminds.internship.teknonymy;
 
 import com.premiumminds.internship.teknonymy.Person;
+import java.util.HashSet;
+import java.util.Set;
 
 class TeknonymyService implements ITeknonymyService {
 
@@ -10,9 +12,7 @@ class TeknonymyService implements ITeknonymyService {
   private static String _secondGenFemale  = "grandmother";
   private static String _prefix           = "great-";
 
-  /* opting for a BFS since we don't know the full depth */
-  private Queue<Person> _queue = new LinkedList<Person>();
-
+  /* NOTE: this smells like a recursive call function to me */
   /**
    * Method to get a Person Teknonymy Name
    * 
@@ -20,6 +20,12 @@ class TeknonymyService implements ITeknonymyService {
    * @return String which is the Teknonymy Name 
    */
   public String getTeknonymy(Person person) {
+    if (person == null) {
+      throw new IllegalArgumentException("Person cannot be null");
+    }
+
+    /* TODO: find a way to not only return the items at the lowest level, but also sort them by gender and age*/
+
     throw new RuntimeException("Not Implemented Yet");
   };
 }
