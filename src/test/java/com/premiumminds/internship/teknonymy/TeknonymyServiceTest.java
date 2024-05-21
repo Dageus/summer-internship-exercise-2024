5ffbd117fe116b719c6ba818a8421a1c0ec7dbb8
Dageus <101069446+Dageus@users.noreply.github.com>
2024-05-21 13:21:03 +0100

    finished tests, seems extensive

--- src/test/java/com/premiumminds/internship/teknonymy/TeknonymyServiceTest.java
+++ src/test/java/com/premiumminds/internship/teknonymy/TeknonymyServiceTest.java
@@ -101,4 +101,29 @@ public class TeknonymyServiceTest {
     String expected = "great-grandfather of Marie";
     assertEquals(expected, result);
   }
+
+  @Test
+  public void PersonWithMultiplePeoplePerGeneration() {
+    Person greatGreatGrandchild1 = new Person("Mikey", 'M', null, LocalDateTime.of(2007, 1, 1, 0, 0));
+    Person greatGreatGrandchild2 = new Person("Candace", 'F', null, LocalDateTime.of(2006, 1, 1, 0, 0));
+    Person greatGrandchild1 = new Person("Jeremy", 'M', new Person[] { greatGreatGrandchild1, greatGreatGrandchild2 },
+        LocalDateTime.of(2003, 1, 1, 0, 0));
+    Person grandchild1 = new Person("Marco", 'M', new Person[] { greatGrandchild1 },
+        LocalDateTime.of(2000, 1, 1, 0, 0));
+    Person grandchild2 = new Person("Ella", 'F', null, LocalDateTime.of(2000, 1, 1, 0, 0));
+    Person greatGreatGrandchild3 = new Person("Marlon", 'M', null, LocalDateTime.of(2007, 1, 2, 0, 0));
+    Person greatGrandchild2 = new Person("Marceline", 'F', new Person[] { greatGreatGrandchild3 },
+        LocalDateTime.of(2004, 1, 1, 0, 0));
+    Person grandchild3 = new Person("Fiona", 'F', new Person[] { greatGrandchild2 },
+        LocalDateTime.of(2000, 1, 1, 0, 0));
+    Person child1 = new Person("Mouse", 'M', new Person[] { grandchild1, grandchild2 },
+        LocalDateTime.of(1990, 1, 1, 0, 0));
+    Person child2 = new Person("Sebastion", 'M', new Person[] { grandchild3 }, LocalDateTime.of(1991, 1, 1, 0, 0));
+    Person child3 = new Person("Jessie", 'F', null, LocalDateTime.of(1993, 1, 1, 0, 0));
+    Person person = new Person("James", 'M', new Person[] { child1, child2, child3 },
+        LocalDateTime.of(1974, 4, 25, 0, 0));
+    String result = new TeknonymyService().getTeknonymy(person);
+    String expected = "great-great-grandfather of Mikey";
+    assertEquals(expected, result);
+  }
 }
